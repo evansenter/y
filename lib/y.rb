99b2e98715fa2cf4e66cde9a80af1ebe5415e453
Evan Senter <evansenter@gmail.com>
2010-03-31 14:24:17 -0400

    Y combinator implementation.

--- lib/y.rb
+++ lib/y.rb
@@ -1,1 +1,17 @@
-# Y
+module Y; end
+
+def Y(call = nil, &block)
+  combinator = proc do |generator|
+    proc { |function| function[function] }.call(proc do |function|
+      generator.call(proc do |value|
+        function[function][value]
+      end)
+    end)
+  end.call(proc do |recurser|
+    proc do |value|
+      yield(recurser, value)
+    end
+  end)
+  
+  call ? combinator[call] : combinator
+end
--- test/y_test.rb
+++ test/y_test.rb
@@ -1,8 +1,13 @@
-require 'test_helper'
+require "test_helper"
+require "Y"
 
 class YTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "combinator with explicit call" do
+    assert_equal 120, Y(5) { |recurser, value| value.zero? ? 1 : value * recurser[value - 1] }
+  end
+  
+  test "combinator when returning function" do
+    factorial = Y { |recurser, value| value.zero? ? 1 : value * recurser[value - 1] }
+    assert_equal 120, factorial[5]
   end
 end
